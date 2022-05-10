62e4359b6b785f2db0719cb9dc76189dbed1c368
Ruchiket Borse <RuchiketBorse1@gmail.com>
2022-05-10 13:25:39 +0530

    generate random hex created

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-// able to swap colours
-// copy to clipboard
-// change the color
-// lock the color
-// save the palette
-// delete the saved palette
-// add animation
+//  Selectors and declarations
+const colorDivs = document.querySelectorAll('.color');
+const generateBtn = document.querySelector('.generate');
+const sliders = document.querySelectorAll('input[type = "range"]');
+const currentHextexes = document.querySelectorAll('.color h2')
+let initialColors;
 
-class Coolors {
-    constructor() {
-        super()
-    }
+
+// Funtions
+
+// - genearate hex
+function randomColor() {
+    let hexColor = chroma.random();
+    return hexColor;
 }
+
+function generateRandomColors() {
+    colorDivs.forEach(div => {
+        let hexText = div.children[0];
+        let randomHex = randomColor();
+        div.style.backgroundColor = randomHex;
+        hexText.innerText = randomHex;
+    })
+}
+
+
+generateRandomColors()
