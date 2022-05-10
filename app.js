0c7d98c91759a6041c57d55e9de2e58fba80a212
Ruchiket Borse <RuchiketBorse1@gmail.com>
2022-05-10 14:08:58 +0530

    check contrast-change text-color added

--- app.js
+++ app.js
@@ -8,20 +8,33 @@ let initialColors;
 
 // Funtions
 
-// - genearate hex
+// - create random hex
 function randomColor() {
     let hexColor = chroma.random();
     return hexColor;
 }
 
+// generate hex and asign to the palette
 function generateRandomColors() {
     colorDivs.forEach(div => {
         let hexText = div.children[0];
         let randomHex = randomColor();
+        // set color and text props as a hex
         div.style.backgroundColor = randomHex;
         hexText.innerText = randomHex;
+        // change text color as per contrast
+        checkContrastText(randomHex, hexText)
     })
 }
 
+// check contrast of color and change color of text on it
+function checkContrastText(color, text) {
+    let luminance = chroma(color).luminance();
+    if (luminance > 0.5) {
+        text.style.color = 'black';
+    } else {
+        text.style.color = 'white'
+    }
+}
 
-generateRandomColors()
+generateRandomColors();
